5715bbb803bac0d68b9453cc13ceff9f931c629c
Andrew Belt <andrewpbelt@gmail.com>
2017-10-28 00:39:50 -0400

    Upgrade from VST1 to VST2, add Mac to build

--- Makefile
+++ Makefile
@@ -1,21 +1,40 @@
+VST2_SDK = VST2_SDK
+
 FLAGS += -Wall -O3 -g -fPIC
-FLAGS += -I$(HOME)/pkg/VST_SDK/VST2_SDK
+FLAGS += -I$(VST2_SDK)
 # Modification to compile the VST SDK with GCC
-FLAGS += -D__cdecl=""
-# FLAGS += -S -save-temps
-CXXFLAGS += $(FLAGS) -std=c++11
-LDFLAGS += -shared
+CXXFLAGS += -std=c++11
+
+ifeq ($(ARCH), mac)
+	MAC_SDK_FLAGS = -mmacosx-version-min=10.7
+	FLAGS += -stdlib=libc++ -arch i386 -arch x86_64 $(MAC_SDK_FLAGS)
+	LDFLAGS += -stdlib=libc++ $(MAC_SDK_FLAGS) -bundle
+	TARGET = VCVBridge
+	BUNDLE = VCVBridge.vst
+endif
+ifeq ($(ARCH), win)
+	LDFLAGS += -shared
+	TARGET = VCVBridge.dll
+endif
+ifeq ($(ARCH), lin)
+	# gcc doesn't use this syntax
+	FLAGS += -D__cdecl=""
+	LDFLAGS += -shared
+	TARGET = VCVBridge.so
+endif
 
 
 SOURCES += vstplugin.cpp
-SOURCES += $(HOME)/pkg/VST_SDK/VST2_SDK/public.sdk/source/vst2.x/audioeffect.cpp
-SOURCES += $(HOME)/pkg/VST_SDK/VST2_SDK/public.sdk/source/vst2.x/audioeffectx.cpp
-SOURCES += $(HOME)/pkg/VST_SDK/VST2_SDK/public.sdk/source/vst2.x/vstplugmain.cpp
+SOURCES += $(VST2_SDK)/public.sdk/source/vst2.x/audioeffect.cpp
+SOURCES += $(VST2_SDK)/public.sdk/source/vst2.x/audioeffectx.cpp
+SOURCES += $(VST2_SDK)/public.sdk/source/vst2.x/vstplugmain.cpp
 
 OBJECTS += $(patsubst %, %.o, $(SOURCES))
 
 
-vstplugin.so: $(OBJECTS)
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
 	$(CXX) -o $@ $^ $(LDFLAGS)
 
 %.c.o: %.c
@@ -27,4 +46,23 @@ vstplugin.so: $(OBJECTS)
 	$(CXX) $(FLAGS) $(CXXFLAGS) -c -o $@ $<
 
 clean:
-	rm -rfv *.o
+	rm -rfv $(OBJECTS) $(TARGET) $(BUNDLE)
+
+dist: all
+ifeq ($(ARCH), mac)
+	mkdir -p $(BUNDLE)/Contents
+	mkdir -p $(BUNDLE)/Contents/MacOS
+	cp Info.plist $(BUNDLE)/Contents/
+	cp PkgInfo $(BUNDLE)/Contents/
+	cp $(TARGET) $(BUNDLE)/Contents/MacOS/
+endif
+
+install: dist
+ifeq ($(ARCH), mac)
+	sudo cp -R $(BUNDLE) /Library/Audio/Plug-Ins/VST/
+endif
+
+uninstall:
+ifeq ($(ARCH), mac)
+	sudo rm -rf /Library/Audio/Plug-Ins/VST/$(BUNDLE)
+endif
--- vstplugin.cpp
+++ vstplugin.cpp
@@ -1,5 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "public.sdk/source/vst2.x/audioeffectx.h"
+
+
+struct Bridge {
+	int channel = 0;
+	void setChannel(int _channel) {
+		channel = _channel;
+	}
+	int getChannel() {
+		return channel;
+	}
+};
 
 
 class Gain : public AudioEffectX {
@@ -15,63 +28,65 @@ public:
 
 	void processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames) override {
 		for (int i = 0; i < sampleFrames; i++) {
-			outputs[0][i] = inputs[0][i] * gain;
-			outputs[1][i] = inputs[1][i] * gain;
+			float r = (float) rand() / RAND_MAX;
+			r = 2.0 * r - 1.0;
+			outputs[0][i] = r; //inputs[0][i];
+			outputs[1][i] = r; //inputs[1][i];
 		}
 	}
 
 	void setParameter(VstInt32 index, float value) override {
 		if (index == 0) {
-			gain = value;
+			bridge.setChannel((int) roundf(value * 63.0));
 		}
 	}
 
 	float getParameter(VstInt32 index) override {
 		if (index == 0) {
-			return gain;
+			return bridge.getChannel() / 63.0;
 		}
 		return 0.0;
 	}
 
 	void getParameterLabel(VstInt32 index, char *label) override {
 		if (index == 0) {
-			snprintf(label, kVstMaxParamStrLen, "Label");
+			snprintf(label, kVstMaxParamStrLen, "");
 		}
 	}
 
 	void getParameterDisplay(VstInt32 index, char *text) override {
 		if (index == 0) {
-			snprintf(text, kVstMaxParamStrLen, "Display");
+			snprintf(text, kVstMaxParamStrLen, "%d", bridge.getChannel());
 		}
 	}
 
 	void getParameterName(VstInt32 index, char *text) override {
 		if (index == 0) {
-			snprintf(text, kVstMaxParamStrLen, "Name");
+			snprintf(text, kVstMaxParamStrLen, "Channel");
 		}
 	}
 
-	bool getEffectName(char* name) {
-		snprintf(name, kVstMaxEffectNameLen, "EffectName");
+	bool getEffectName(char* name) override {
+		snprintf(name, kVstMaxEffectNameLen, "VCV Bridge");
 		return true;
 	}
 
-	bool getVendorString(char* text) {
-		snprintf(text, kVstMaxProductStrLen, "VendorString");
+	bool getVendorString(char* text) override {
+		snprintf(text, kVstMaxProductStrLen, "VCV");
 		return true;
 	}
 
-	bool getProductString(char* text) {
+	bool getProductString(char* text) override {
 		snprintf(text, kVstMaxVendorStrLen, "ProductString");
 		return true;
 	}
 
-	VstInt32 getVendorVersion() {
+	VstInt32 getVendorVersion() override {
 		return 0;
 	}
 
 private:
-	float gain = 0.0;
+	Bridge bridge;
 };
 
 
